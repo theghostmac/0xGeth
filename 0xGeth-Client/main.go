6b02bbba146780d8e639bf365ceaa2a1d3c7c061
MacBobby Chibuzor <theghostmac@gmail.com>
2023-04-15 04:33:32 +0100

    gitignored .idea files | created eth client

---
+++ 0xGeth-Client/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"log"
+)
+
+var infuraURl = "https://mainnet.infura.io/v3/66c41c21577b45b0848e04dc31fe30d4"
+
+func main() {
+	// create the eth client
+	ethclient.DialContext(context.Background(), infuraURL)
+	if err != nil {
+		log.Fatalf("Error creating ethereum client: %v", err)
+	}
+	defer client.Close()
+	block, err := client.BlockByNumber(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error checking block number: %v", err)
+	}
+	fmt.Println(block.Number())
+}
+
+// cmd: curl --url https://mainnet.infura.io/v3/66c41c21577b45b0848e04dc31fe30d4 \
+//-X POST \
+//-H "Content-Type: application/json" \
+//-d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 0xGeth
-tries to interact with Ethereum with Go.
+In this project to interact with Ethereum with Go.
+
+## Tasks
+* [ ] Interact with Ethereum using Go
+  * [ ] Use infura.io service to interact the main blockchain.
